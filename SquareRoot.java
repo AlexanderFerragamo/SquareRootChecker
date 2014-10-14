de876cc87b75b5fd42f10d4c9f8dd4d17eacacbe
AlexanderFerragamo <alexander_ferragamo@asl.org>
2014-10-14 09:26:37 +0100

    SquareRootChecker AF

---
+++ SquareRoot.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class SquareRoot{
+
+   public static void main(String[] args){
+      Scanner scnr = new Scanner(System.in);
+      System.out.println("Input an integer: ");
+      int int1 = scnr.nextInt();
+      if (int1 > -1){
+      System.out.println(Math.sqrt(int1));
+      } else {
+      System.out.println("Error!");
+      }
+      
+      
+      
+      }
+      }
+      
