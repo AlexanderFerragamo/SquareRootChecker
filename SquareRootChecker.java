62d5c7d5a766f2804aef75f7905081a7dca21d93
AlexanderFerragamo <alexander_ferragamo@asl.org>
2014-10-14 09:01:08 +0100

    SquareRootChecker

--- SquareRootChecker.java
+++ SquareRootChecker.java
@@ -2,15 +2,26 @@
 *
 * Prompts user to enter a value, checks if negative -- if not, prints out the square root
 *
-* @author <your name here>
-* @version <date you finished the code>
+* @author <Alexander Ferragamo>
+* @version <october 14>
 *
 */
 
-public class SquareRootChecker {
-	
-	public static void main(String[] args) {
-		// TODO - Put your code here
-	}
+import java.util.Scanner;
 
-}
+public class SquareRootChecker{
+
+   public static void main(String[] args){
+      Scanner scnr = new Scanner(System.in);
+      System.out.println("Input an integer: ");
+      int int1 = scnr.nextInt();
+      if (int1 > -1){
+      System.out.println(Math.sqrt(int1));
+      } else {
+      System.out.println("Error!");
+      }
+      
+      
+      
+      }
+      }
